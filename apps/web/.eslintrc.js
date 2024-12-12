d37e669095b56f241dd0cdc507bb0f7dbbd07350
Jorge Machado <jorge.vmachado@hotmail.com>
2024-12-12 14:55:20 -0300

    change name packages/eslint-config to packages/eslint

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,6 +1,6 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ['@repo/eslint-config/next.js'],
+  extends: ['@repo/eslint/next.js'],
   parserOptions: {
     project: true,
   },

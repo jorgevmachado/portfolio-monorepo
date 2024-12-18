ac9182786867d59efdbbaf511fde8ea40f1e7226
Jorge Machado <jorge.vmachado@hotmail.com>
2024-12-18 09:49:09 -0300

    adjusting the fixtures and business rule in the business project

--- packages/business/src/index.ts
+++ packages/business/src/index.ts
@@ -1,17 +1,3 @@
-import { Link } from 'links/entities/link.entity';
-
-import { CreateLinkDto } from 'links/dto/create-link.dto';
-import { UpdateLinkDto } from 'links/dto/update-link.dto';
-
-export const links = {
-  dto: {
-    CreateLinkDto,
-    UpdateLinkDto,
-  },
-  entities: {
-    Link,
-  },
-};
 export * from './api'
 export * from './auth';
 export * from './pokemon';

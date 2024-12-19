fd823c4d5b133741949b37c7b031c43242758112
Jorge Machado <jorge.vmachado@hotmail.com>
2024-12-19 00:23:11 -0300

    Implementation of authentication

--- apps/api/src/pokemons/pokemon.module.ts
+++ apps/api/src/pokemons/pokemon.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Pokemon as PokemonBusiness } from '@repo/business/pokemon/pokemon';
+
 import { PokemonService } from './pokemon.service';
 import { PokemonController } from './pokemon.controller';
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { Pokemon } from "./entities/pokemon.entity";
-import { Pokemon as PokemonBusiness } from "@repo/business/pokemon/pokemon";
+import { Pokemon } from './entities/pokemon.entity';
 import { TypeModule } from './type/type.module';
 import { MoveModule } from './move/move.module';
 import { AbilityModule } from './ability/ability.module';
@@ -11,6 +14,7 @@ import { AbilityModule } from './ability/ability.module';
 @Module({
   imports: [
     TypeOrmModule.forFeature([Pokemon]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     TypeModule,
     MoveModule,
     AbilityModule,

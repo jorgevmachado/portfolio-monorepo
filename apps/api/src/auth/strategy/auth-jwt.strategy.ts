264287e2f625cded964f3bbf46dae2efd4dcffb2
Jorge Machado <jorge.vmachado@hotmail.com>
2024-12-19 00:18:07 -0300

    Implementation of authentication

---
+++ apps/api/src/auth/strategy/auth-jwt.strategy.ts
@@ -0,0 +1,32 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+import { Repository } from 'typeorm';
+
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PassportStrategy } from '@nestjs/passport';
+
+import {User} from "../users/user.entity";
+
+@Injectable()
+export class AuthJwtStrategy extends PassportStrategy(Strategy) {
+    constructor(
+        @InjectRepository(User)
+        private repository: Repository<User>,
+    ) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: 'super-secret',
+        });
+    }
+
+    async validate(payload: { id: string }) {
+        const user = await this.repository.findOne({ where: { id: payload.id } });
+
+        if (!user) {
+            throw new UnauthorizedException('User not found!');
+        }
+
+        return user;
+    }
+}

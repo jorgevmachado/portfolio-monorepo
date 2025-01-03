dde581446096023d9894ddce28dce0087f86bc46
Jorge Machado <jorge.vmachado@hotmail.com>
2025-01-03 16:36:00 -0300

    element image

---
+++ packages/ds/src/elements/image/Image.stories.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Image from './Image';
+
+const src =
+    'https://w7.pngwing.com/pngs/173/127/png-transparent-geek-logo-graphy-others-photography-artwork-sales-thumbnail.png';
+
+const meta = {
+    args: {
+        fit: undefined,
+        lazyLoad: false,
+        fallback: true,
+    },
+    title: 'Elements/Image',
+    argTypes: {
+        fit: {
+            table: {
+                type: { summary: 'string' },
+                defaultValue: { summary: 'cover' },
+            },
+            options: ['cover', 'contain'],
+            control: { type: 'radio' },
+        },
+        lazyLoad: {
+            table: {
+                type: { summary: 'boolean' },
+                defaultValue: { summary: 'false' },
+            },
+        },
+        fallback: {
+            table: {
+                type: { summary: 'boolean' },
+                defaultValue: { summary: 'true' },
+            },
+        },
+    },
+    component: Image,
+} satisfies Meta<typeof Image>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: { src }
+};
--- packages/ds/src/elements/index.ts
+++ packages/ds/src/elements/index.ts
@@ -3,3 +3,4 @@ export { default as Skeleton } from './skeleton';
 export { default as Spinner } from './spinner';
 export { default as Tooltip } from './tooltip';
 export { default as Text } from './text';
+export { default as Image } from './image';

86869cbb93d4d5131ec5bffdfee1ed59666143fa
Jorge Machado <jorge.vmachado@hotmail.com>
2025-01-06 20:47:05 -0300

    component input

---
+++ packages/ds/src/components/input/interface.ts
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import type { TContext } from '../../utils/colors';
+
+type InputPropsItem = Pick<
+    React.HTMLProps<Element>,
+    'onBlur' |
+    'onInput' |
+    'onFocus' |
+    'onChange' |
+    'onKeyDown' |
+    'onMouseDown'
+>;
+
+type HostProps = Omit<React.HTMLProps<HTMLDivElement>, keyof InputPropsItem>;
+
+export interface InputProps extends InputPropsItem, HostProps {
+    tip?: string;
+    type?: string;
+    addon?: string;
+    value?: string;
+    label?: string;
+    variant?: 'regular' | 'large';
+    disabled?: boolean;
+    multiline?: boolean;
+    isInvalid?: boolean;
+    autoFocus?: boolean;
+    dataCyName?: string;
+    helperText?: React.ReactNode;
+    iconContext?: TContext;
+    floatingLabel?: boolean;
+    invalidMessage?: string;
+    hasFloatingSlots?: boolean;
+}
